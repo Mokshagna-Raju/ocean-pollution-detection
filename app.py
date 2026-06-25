6e702347796c8fa576d4c58f3d99dc1f3527783a
Mokshagna Raju <rajubm193@gmail.com>
2026-06-25 15:47:37 +0530

    Initialize database on startup

--- app.py
+++ app.py
@@ -314,7 +314,7 @@ def view_result(upload_id):
         lon=lon
     )
 
+init_db()
 
-if __name__ == "__main__":
-    init_db()
+if __name__ == "__main__":    
     app.run(host="0.0.0.0", port=5000 )
